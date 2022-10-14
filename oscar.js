6635b89b3ec014bb5b7f982c71d07da12717ef83
Ayotayo <ayotayobadejo@gmail.com>
2022-10-14 05:22:16 -0700

    e-comerce commit

---
+++ oscar.js
@@ -0,0 +1,21 @@
+// var obj = [
+//     {
+//         username: 'admin',
+//         password: 'admin11',
+//     },{
+//         username: 'admins',
+//         password: 'admin113',
+//     },{
+//         username: 'adminos',
+//         password: 'admin1100',
+//     }
+// ]
+// function get(){
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     for(i = 0; i < obj.length; i++) {
+//         if(username == obj[i].username && password == obj[i].password) {
+//             console.log('youre logged in')
+//         }
+//     }
+// }
